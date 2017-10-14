23142e9f3a36187c34d252b4633d53081234563b
Moritz Meier <moritzmeier@aol.com>
2017-10-14 21:40:17 +0200

    unicode rendering to separate file

--- falten.py
+++ falten.py
@@ -8,207 +8,69 @@ with a fixed bitmap font like GNU Unifont Mono.
 
 from itertools import cycle, chain
 
-try:
-    import colored
-    color_available = True
-except Exception as e:
-    print(e)
-    print('Install with: ´pip install colored´')
-    color_available = False
+from svg_falts import SvgFold
+from unicode_falts import to_2d_repr, color_available, colorize
 
 
-default_plate = ("red green yellow blue magenta cyan light_red light_green "
+default_plate_unicode = ("red green yellow blue magenta cyan light_red light_green "
     + "light_yellow light_blue light_magenta light_cyan dark_gray light_gray")
 
-bottom_left = '╰' #'└'
-top_right = '╮' #'┐'
-horizontal = '─'
-vertical = '│'
-bottom_right = '╯' #'┘'
-top_left = '╭' #'┌'
-
-corners = (bottom_left, top_right, bottom_right, top_left)
-straigts = (horizontal, vertical)
-
-
-def to_2d_repr(s):
-    s = s.replace('.', ',')
-    s = s.replace(';', ',')
-    blocks = s.split(',')
-    pairs = [process_one_block(b) for b in blocks]
-    tops, bottoms = zip(*pairs)
-
-    # extend to longest block
-    mlen = max([len(t) for t in tops]) + max([len(b) for b in bottoms])
-    for top,bot in zip(tops,bottoms):
-        extra_layers = [vertical*len(top[0])]*(mlen-len(top)-len(bot))
-        #print(top,'add',mlen-len(top))
-        top.extend(extra_layers)
-
-    # join bottoms and tops
-    block = [t + b for t, b in zip(tops, bottoms)]
-
-    # join rows
-    block = [' '.join(row) for row in zip(*block)]
-
-    block = '\n'.join(block)
-
-    return block
-
-
-def process_one_block(s):
-    s = s.replace('.','')
-    if s == '|':
-        return [vertical], [vertical]
-    if len(s)%2!=0:
-        raise Exception('Not even: ' + s)
-    s = s.replace('|', vertical)
-    s = s.replace('(', top_left)
-    s = s.replace(')', top_right)
-
-    layers = []
-    construct_layers(s, layers)
-    correct_layers(layers)
-    layers = [''.join(l) for l in layers]
-
-    hl = len(s) // 2
-    bottom = [
-        vertical * (hl - i - 1) +
-        bottom_left +
-        horizontal * (2 * i) +
-        bottom_right +
-        vertical * (hl - i - 1)
-        for i in range(hl)
-    ]
-    return layers, bottom
-
-
-def correct_layers(layers):
-    for i in range(1,len(layers)):
-        for j in range(len(layers[i])):
-            if layers[i][j] == horizontal:
-                if layers[i-1][j] in [vertical,top_left,top_right]:
-                    layers[i][j] = vertical
-
-
-def construct_layers(s, layers, lay_num=0, i=0):
-    if lay_num == len(layers):
-        layers.append([horizontal] * len(s))
-    layer = layers[lay_num]
-    while i<len(s):
-        if s[i] == vertical:
-            layer[i] = vertical
-        elif s[i] == top_left:
-            layer[i] = top_left
-            i = construct_layers(s, layers, lay_num + 1, i + 1)
-            layer[i] = top_right
-        elif s[i] == top_right:
-            return i
-        i+=1
-
-
-def _next_direction(segment, direction):
-    if segment == top_left:
-        if direction == 'up':
-            direction = 'right'
-        if direction == 'left':
-            direction = 'down'
-    if segment == top_right:
-        if direction == 'up':
-            direction = 'left'
-        if direction == 'right':
-            direction = 'down'
-    if segment == bottom_left:
-        if direction == 'down':
-            direction = 'right'
-        if direction == 'left':
-            direction = 'up'
-    if segment == bottom_right:
-        if direction == 'down':
-            direction = 'left'
-        if direction == 'right':
-            direction = 'up'
-
-    return direction
-
-
-def colorize(pipes, plate, background=False):
-    """Colorizes each individual edge."""
-
-    if background:
-        color_func = colored.bg
-    else:
-        color_func = colored.fg
-
-    lines = pipes.split('\n')
-    pixels = list(map(list, lines))
-    already_colored = []
-
-    for i, segment in enumerate(lines[0]):
-        if (segment != vertical) or (i in already_colored):
-            continue
-        direction = 'down'
-        color = next(plate)
-        y = 0
-        x = i
-        while 0 <= y < len(lines):
-            segment = pixels[y][x]
-            pixel = color_func(color)
-            pixel += segment + colored.attr('reset')
-            pixels[y][x] = pixel
-
-            direction = _next_direction(segment, direction)
-
-            if direction == 'down':
-                y += 1
-            elif direction == 'up':
-                y -= 1
-            elif direction == 'right':
-                x += 1
-            elif direction == 'left':
-                x -= 1
-
-            #print(segment, direction, color, x, y)
-
-        already_colored.append(x)
-
-    return '\n'.join(''.join(line) for line in pixels)
-
+default_plate_svg = ("red green blue magenta cyan dark_yellow light_red light_green "
+    + "light_blue light_magenta light_cyan dark_gray light_gray")
 
 def main():
     import argparse
     import sys
 
     parser = argparse.ArgumentParser(
-        description="Converts bar-bra notation for the side views of cross-folded paper to a 2-dimensional terminal output. For color options look at: github.com/dslackw/colored",
-        epilog= 'Example: python falten.py -p "cyan magenta blue" "||.||().||.||()||()"')
-    parser.add_argument('--plate', '-p', metavar="str",
-        default=default_plate,
-        help="Space separated string of colors.")
-    parser.add_argument('-b', '--background', action='store_true', help="Color background instead of foreground.")
-    #parser.add_argument('-B', '--bold', action='store_true', help="Draw all lines bold.")
-    parser.add_argument('barbra', nargs='*', default=sys.stdin,
-        help="Cross-fold in bar-bra notation.")
+        description = "Converts bar-bra notation for the side views of cross-folded paper to a 2-dimensional terminal output. For color options look at: github.com/dslackw/colored",
+        epilog = 'Example: python falten.py -p "cyan magenta blue" "||.||().||.||()||()"',
+    )
+    parser.add_argument('--plate', '-p',
+        metavar = "str",
+        help = "Space separated string of colors.",
+    )
+    parser.add_argument('-b', '--background',
+        action = 'store_true',
+        help = "Color background instead of foreground.",
+    )
+    parser.add_argument('--svg',
+        action = 'store_true',
+        help = "Reder output as SVG. (experimental feature)",
+    )
+    parser.add_argument('barbra',
+        nargs = '*',
+        default = sys.stdin,
+        help = "Cross-fold in bar-bra notation.",
+    )
     args = parser.parse_args()
 
+    # this part is for piping
     if args.barbra == sys.stdin:
         args.barbra = args.barbra.readlines()
 
     falt_iter = (string.split() for string in args.barbra)
     falt_iter = filter(bool, chain(*falt_iter))
 
-    if args.plate and color_available:
-        plate = cycle(args.plate.split())
+    if args.plate == None:
+        plate = default_plate_svg if args.svg else default_plate_unicode
+    else:
+        plate = args.plate
 
-    for bb in falt_iter:
-        falt_2d_str = to_2d_repr(bb)
-        if args.plate and color_available:
-            falt_2d_str = colorize(falt_2d_str, plate, args.background)
-        print(falt_2d_str, end='\n')
+    plate = cycle(plate.split())
 
-def print_colors():
-    names = (n.lower() for n in colored.names)
-    print(' '.join(colored.fg(n) + n + colored.attr('reset') for n in names))
+    if args.svg:
+        for bb in falt_iter:
+            img = SvgFold(bb, plate=plate)
+            svg_code = img.render()
+            print(svg_code)
+    else:
+        for bb in falt_iter:
+            falt_2d_str = to_2d_repr(bb)
+            if plate and color_available:
+                falt_2d_str = colorize(falt_2d_str, plate, args.background)
+            print(falt_2d_str, end='\n')
+
 
 if __name__ == '__main__':
     main()
---
+++ unicode_falts.py
@@ -0,0 +1,161 @@
+try:
+    import colored
+    color_available = True
+except Exception as e:
+    print(e)
+    print('Terminal coloring not available!')
+    print('Install with: ´pip install colored´')
+    color_available = False
+
+bottom_left = '╰' #'└'
+top_right = '╮' #'┐'
+horizontal = '─'
+vertical = '│'
+bottom_right = '╯' #'┘'
+top_left = '╭' #'┌'
+
+def to_2d_repr(s):
+    s = s.replace('.', ',')
+    s = s.replace(';', ',')
+    blocks = s.split(',')
+    pairs = [process_one_block(b) for b in blocks]
+    tops, bottoms = zip(*pairs)
+
+    # extend to longest block
+    mlen = max([len(t) for t in tops]) + max([len(b) for b in bottoms])
+    for top,bot in zip(tops,bottoms):
+        extra_layers = [vertical*len(top[0])]*(mlen-len(top)-len(bot))
+        #print(top,'add',mlen-len(top))
+        top.extend(extra_layers)
+
+    # join bottoms and tops
+    block = [t + b for t, b in zip(tops, bottoms)]
+
+    # join rows
+    block = [' '.join(row) for row in zip(*block)]
+
+    block = '\n'.join(block)
+
+    return block
+
+def process_one_block(s):
+    s = s.replace('.','')
+    if s == '|':
+        return [vertical], [vertical]
+    if len(s) % 2 != 0:
+        raise Exception('Not even: ' + s)
+    s = s.replace('|', vertical)
+    s = s.replace('(', top_left)
+    s = s.replace(')', top_right)
+
+    layers = []
+    construct_layers(s, layers)
+    correct_layers(layers)
+    layers = [''.join(l) for l in layers]
+
+    hl = len(s) // 2
+    bottom = [
+        vertical * (hl - i - 1) +
+        bottom_left +
+        horizontal * (2 * i) +
+        bottom_right +
+        vertical * (hl - i - 1)
+        for i in range(hl)
+    ]
+    return layers, bottom
+
+def correct_layers(layers):
+    for i in range(1,len(layers)):
+        for j in range(len(layers[i])):
+            if layers[i][j] == horizontal:
+                if layers[i-1][j] in [vertical,top_left,top_right]:
+                    layers[i][j] = vertical
+
+
+def construct_layers(s, layers, lay_num=0, i=0):
+    if lay_num == len(layers):
+        layers.append([horizontal] * len(s))
+    layer = layers[lay_num]
+    while i<len(s):
+        if s[i] == vertical:
+            layer[i] = vertical
+        elif s[i] == top_left:
+            layer[i] = top_left
+            i = construct_layers(s, layers, lay_num + 1, i + 1)
+            layer[i] = top_right
+        elif s[i] == top_right:
+            return i
+        i+=1
+
+
+def _next_direction(segment, direction):
+    if segment == top_left:
+        if direction == 'up':
+            direction = 'right'
+        if direction == 'left':
+            direction = 'down'
+    if segment == top_right:
+        if direction == 'up':
+            direction = 'left'
+        if direction == 'right':
+            direction = 'down'
+    if segment == bottom_left:
+        if direction == 'down':
+            direction = 'right'
+        if direction == 'left':
+            direction = 'up'
+    if segment == bottom_right:
+        if direction == 'down':
+            direction = 'left'
+        if direction == 'right':
+            direction = 'up'
+
+    return direction
+
+
+def colorize(pipes, plate, background=False):
+    """Colorizes each individual edge."""
+
+    if background:
+        color_func = colored.bg
+    else:
+        color_func = colored.fg
+
+    lines = pipes.split('\n')
+    pixels = list(map(list, lines))
+    already_colored = []
+
+    for i, segment in enumerate(lines[0]):
+        if (segment != vertical) or (i in already_colored):
+            continue
+        direction = 'down'
+        color = next(plate)
+        y = 0
+        x = i
+        while 0 <= y < len(lines):
+            segment = pixels[y][x]
+            pixel = color_func(color)
+            pixel += segment + colored.attr('reset')
+            pixels[y][x] = pixel
+
+            direction = _next_direction(segment, direction)
+
+            if direction == 'down':
+                y += 1
+            elif direction == 'up':
+                y -= 1
+            elif direction == 'right':
+                x += 1
+            elif direction == 'left':
+                x -= 1
+
+            #print(segment, direction, color, x, y)
+
+        already_colored.append(x)
+
+    return '\n'.join(''.join(line) for line in pixels)
+
+
+def print_colors():
+    names = (n.lower() for n in colored.names)
+    print(' '.join(colored.fg(n) + n + colored.attr('reset') for n in names))
